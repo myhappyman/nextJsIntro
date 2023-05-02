820dc51ffbd2a16c2aec4a01831f8e982e5ebf32
Park_Shinwoo <myhappyman@naver.com>
2023-05-02 16:41:46 +0900

    next를 쓰게되면 얻는 잇점들로 인터넷이 느린환경이나 검색노출이 잘되도록 페이지를 작성할 수 있고 기본적인 구조를 생성시켜준다.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,12 @@
+import { useState } from "react";
+
 export default function Home(){
+    const [couter, setCouter] = useState(0);
     return (
         <div>
-            <h1>Home</h1>
+            <h1>Home {couter}</h1>
+
+            <button onClick={() => setCouter(prev => prev+1)}>+</button>
         </div>
     );
 }
